5714db24dc6644b645c6d01aa81d9e9833b98898
dino <dino@beansmile.com>
2019-05-07 16:56:34 +0800

    remove baidu-tongji related

--- lib/bl_commons/engine.rb
+++ lib/bl_commons/engine.rb
@@ -1,7 +1,5 @@
 require_relative 'bl_resources'
 require_relative 'bl_resources/resource_client'
-require_relative 'baidu_tongji'
-require_relative 'baidu_tongji/baidu_tongji_client'
 
 module BlCommons
   class Engine < ::Rails::Engine
