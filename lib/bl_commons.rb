11d4bd04d753e09095539af1706c44d645ab121d
ShallmentMo <ShallmentMo@gmail.com>
2019-06-13 14:17:01 +0800

    share redis

--- lib/bl_commons.rb
+++ lib/bl_commons.rb
@@ -1,5 +1,27 @@
+# frozen_string_literal: true
+
 require 'bl_commons/engine'
 
 module BlCommons
-  # Your code goes here...
+  def self.redis
+    return @redis if @redis
+
+    redis_url = Rails.application.secrets[:redis_url]
+    redis_password = Rails.application.secrets.redis[:password]
+
+    @redis = Redis::Namespace.new(
+      :bl_commons,
+      redis: Redis.new(url: redis_url, password: redis_password)
+    )
+  end
+
+  def self.available_locales
+    return [I18n.default_locale.to_s] unless redis.get('available_locales')
+
+    JSON.parse(redis.get('available_locales'))
+  end
+
+  def self.available_locales=(array)
+    redis.set('available_locales', array)
+  end
 end
