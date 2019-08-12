0a690b15c732606b7626e262ed2d669910567ce3
ShallmentMo <ShallmentMo@gmail.com>
2019-08-12 19:05:04 +0800

    sync resources with batch size

--- app/jobs/bl_commons/sync_resources_job.rb
+++ app/jobs/bl_commons/sync_resources_job.rb
@@ -3,22 +3,24 @@
 module BlCommons
   class SyncResourcesJob < ApplicationJob
     def perform(model_name, ids)
-      collection = model_name.constantize.where(id: ids)
+      relation = model_name.constantize.where(id: ids)
 
-      return unless collection.exists?
+      return unless relation.exists?
 
-      collection.send(:bl_sync_resource_nodes).each do |node|
-        resp = node.batch_sync(
-          "/#{collection.bl_sync_resource_name}",
-          collection: collection.map do |object|
-            {
-              object.bl_sync_resource_foreign_key.to_s => object.id,
-              resource_params: object.send(:bl_sync_resource_params)
-            }
-          end.to_a
-        )
+      relation.in_batches(of: relation.bl_sync_resource_batch_size) do |collection|
+        collection.send(:bl_sync_resource_nodes).each do |node|
+          resp = node.batch_sync(
+            "/#{collection.bl_sync_resource_name}",
+            collection: collection.map do |object|
+              {
+                object.bl_sync_resource_foreign_key.to_s => object.id,
+                resource_params: object.send(:bl_sync_resource_params)
+              }
+            end.to_a
+          )
 
-        raise "#{node.name}: 同步 #{model_name} #{ids} 失败，#{resp['error_message']}" if resp['error_message']
+          raise "#{node.name}: 同步 #{model_name} #{ids} 失败，#{resp['error_message']}" if resp['error_message']
+        end
       end
     end
   end
